987c45845814de640e44d02cd74b80825da458a0
Andrew <fangggandrew@gmail.com>
2022-09-01 01:24:21 +0800

    Updated some DBs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from pythonFiles.customer_accounts import Login, Register, Email, OTP, ResetPassword, UpdateDetail
+from pythonFiles.customer_accounts import Login, Register, Email, OTP, ResetPassword, UpdateDetail, CurrentPassword
 from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -21,6 +21,7 @@ mail=Mail(app)
 db = SQLAlchemy(app)
 
 
+# Andrew - User's database
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     account_type = db.Column(db.String(10), nullable=False)
@@ -36,11 +37,26 @@ class Users(db.Model):
         return check_password_hash(self.password, password)
 
 
+class Staffs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account_type = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
+# Andrew - Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     log_in = Login(request.form)
@@ -55,7 +71,7 @@ def login():
             if check:
                 log_in.email.data = ''
                 log_in.password.data = ''
-                session['account_type'] = 'User'
+                session['account_type'] = pw_to_check.account_type
                 session['user id'] = pw_to_check.id
                 return redirect(url_for("account_detail"))
             else:
@@ -65,16 +81,24 @@ def login():
     return render_template("login and register/login.html", form=log_in)
 
 
+# Andrew - Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register = Register(request.form)
     if request.method == 'POST' and register.validate():
         user = Users.query.filter_by(email=register.email.data).first()
-        if user is None:
-            user = Users(account_type='User', name=register.name.data, email=register.email.data)
-            user.set_password(register.password.data)
-            db.session.add(user)
-            db.session.commit()
+        if register.password.data == register.confirm_password.data:
+            if user is None:
+                user = Users(account_type='User', name=register.name.data, email=register.email.data)
+                user.set_password(register.password.data)
+                db.session.add(user)
+                db.session.commit()
+            else:
+                flash("Email exists already. Please ", 'emailexists')
+                return render_template("login and register/register.html", form=register)
+        else:
+            flash("Confirm password not the same as password. Please try again.", 'wrongpassword')
+            return render_template("login and register/register.html", form=register)
         register.email.data = ''
         register.password.data = ''
         session['account_updated'] = "Account Created Successfully!"
@@ -82,6 +106,7 @@ def register():
     return render_template("login and register/register.html", form=register)
 
 
+# Andrew - User account detail
 @app.route('/account_detail', methods=['GET', 'POST'])
 def account_detail():
     updateUser = UpdateDetail(request.form)
@@ -112,22 +137,25 @@ def account_detail():
     return render_template("customer accounts/account_detail.html", form=updateUser)
 
 
+# Andrew - User redemption history
 @app.route('/redemption_history')
 def redemption_history():
     return render_template('customer accounts/redemption_history.html')
 
 
+# Andrew - User recycle history
 @app.route('/recycle_history', methods=['GET', 'POST'])
 def recycle_history():
     users = Users.query.order_by(Users.id)
     return render_template('customer accounts/recycle_history.html', users=users)
 
 
+# Andrew - Guest Forgot Password
 @app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
     forgot_password = Email(request.form)
     email = forgot_password.email.data
-    session['forgot_email'] = email
+    session['reset_email'] = email
     user_detail = Users.query.filter_by(email=email).first()
     if request.method == 'POST' and forgot_password.validate():
         if user_detail:
@@ -135,7 +163,7 @@ def forgot_password():
             session['otp'] = otp
             msg = Message(subject="Climate Change Account Authentication", sender="tackleclimatechanges@gmail.com",
                           recipients=[email])
-            msg.body = "Dear Customer, please use the code:"+str(otp)+" for your email authentication. Thank you."
+            msg.body = "Dear User, please use the code:"+str(otp)+" for your email authentication. Thank you."
             session['otp_verification'] = f'Code sent to {email}. Please enter the code for authentication.'
             mail.send(msg)
             return redirect(url_for('otp_verification'))
@@ -144,31 +172,109 @@ def forgot_password():
     return render_template('customer accounts/forgot_password/forgot_password.html', form=forgot_password)
 
 
+# Andrew - OTP Verification
 @app.route('/otp_verification', methods=['GET', 'POST'])
 def otp_verification():
     otp_verification = OTP(request.form)
     if request.method == 'POST' and otp_verification.validate():
         if otp_verification.otp.data == session['otp']:
             session['authenticated'] = "Authentication successful."
-            return redirect(url_for('reset_password'))
+            if 'delete_account' in session:
+                return redirect(url_for('confirm_deletion'))
+            else:
+                return redirect(url_for('reset_password'))
         else:
             flash("Wrong OTP code.")
     return render_template('customer accounts/forgot_password/otp_verification.html', form=otp_verification)
 
 
+# Andrew - Guest reset password
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_password():
     reset_password = ResetPassword(request.form)
     if request.method == 'POST' and reset_password.validate():
         if reset_password.new_password.data == reset_password.confirm_password.data:
-            user_detail = Users.query.filter_by(email=session['forgot_email']).first()
+            user_detail = Users.query.filter_by(email=session['reset_email']).first()
             user_detail.set_password(reset_password.new_password.data)
             db.session.commit()
             session['account_updated'] = "Account password has been updated successfully."
-            return redirect(url_for('login'))
+            if 'account_reset_password' in session:
+                return redirect(url_for('account_detail'))
+            else:
+                return redirect(url_for('login'))
         else:
             flash("Please ensure your new and confirm password is the same.")
     return render_template('customer accounts/forgot_password/reset_password.html', form=reset_password)
+
+
+# Andrew - User's reset password
+@app.route('/account_reset_password', methods=['GET', 'POST'])
+def account_reset_password():
+    account_reset_password = CurrentPassword(request.form)
+    password = account_reset_password.password.data
+    user_detail = Users.query.filter_by(id=session['user id']).first()
+    email = user_detail.email
+    if request.method == 'POST' and account_reset_password.validate():
+        check = check_password_hash(user_detail.password, password)
+        if check:
+            otp = randint(100000, 999999)
+            session['otp'] = otp
+            msg = Message(subject="Climate Change Account Authentication", sender="tackleclimatechanges@gmail.com",
+                          recipients=[email])
+            msg.body = "Dear User, please use the code:"+str(otp)+" for your email authentication. Thank you."
+            session['otp_verification'] = f'Code sent to {email}. Please enter the code for authentication.'
+            mail.send(msg)
+            session['account_reset_password'] = True
+            session['reset_email'] = user_detail.email
+            return redirect(url_for('otp_verification'))
+        else:
+            flash("Wrong Password.")
+    return render_template('customer accounts/reset_password/current_password.html', form=account_reset_password)
+
+
+# Andrew - User's reset password
+@app.route('/delete_account', methods=['GET', 'POST'])
+def delete_account():
+    delete_account = Email(request.form)
+    user_detail = Users.query.filter_by(id=session['user id']).first()
+    email = user_detail.email
+    if request.method == 'POST' and delete_account.validate():
+        if delete_account.email.data == user_detail.email:
+            otp = randint(100000, 999999)
+            session['otp'] = otp
+            msg = Message(subject="Climate Change Account Authentication", sender="tackleclimatechanges@gmail.com",
+                          recipients=[email])
+            msg.body = "Dear User, please use the code:"+str(otp)+" for your email authentication. Thank you."
+            session['otp_verification'] = f'Code sent to {email}. Please enter the code for authentication.'
+            mail.send(msg)
+            session['delete_account'] = True
+            session['reset_email'] = email
+            return redirect(url_for('otp_verification'))
+        else:
+            flash("Wrong Email.")
+    return render_template('customer accounts/delete_account/check_email.html', form=delete_account)
+
+
+# @app.route('/confirm_deletion/<int:id>')
+# def confirm_deletion():
+#
+#     return redirect(url_for('home'))
+
+
+@app.route('/confirm_deletion')
+def confirm_deletion():
+    user = Users.query.filter_by(id=session['user id']).first()
+    return render_template('customer accounts/delete_account/confirm_deletion.html', user=user)
+
+
+@app.route('/delete_account_confirmed')
+def delete_account_confirmed():
+    user = Users.query.filter_by(id=session['user id']).first()
+    db.session.delete(user)
+    db.session.commit()
+    session.clear()
+    session['account_updated'] = "Account has been successfully deleted."
+    return redirect(url_for('login'))
 
 
 @app.route('/logout')
